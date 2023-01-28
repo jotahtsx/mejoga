a1d5c060fbd944299c482d23c56a52c1c9311164
jotahtsx <joaomanoelskai@gmail.com>
2023-01-28 09:24:28 -0300

    mejoga || Add - Google Font

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -39,7 +39,18 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="pt-BR">
-        <Head />
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin=""
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Work+Sans:wght@300;400;500;600;700;800;900&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
