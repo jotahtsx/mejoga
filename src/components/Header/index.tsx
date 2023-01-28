9aa3af510237138d4e7c10475b57e6e21750a262
jotahtsx <joaomanoelskai@gmail.com>
2023-01-28 12:01:29 -0300

    mejoga || Add  - Storybook

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,5 @@
+import * as H from './styles'
+
+const Header = () => <H.Wrapper>header</H.Wrapper>
+
+export default Header
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,11 @@
+import Header from 'components/Header'
 import type { AppProps } from 'next/app'
 import { GlobalStyle } from 'styles/global'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Header />
       <Component {...pageProps} />
       <GlobalStyle />
     </>
