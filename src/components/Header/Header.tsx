906256a9de976de93af67eed4ff415b79b388725
jotahtsx <joaomanoelskai@gmail.com>
2023-01-29 18:55:34 -0300

    mejoga || Add - Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,32 @@
+import Link from 'next/link'
+import Image from 'next/image'
+import Brand from '../../../public/images/brand.svg'
 import * as H from './styles'
 
 export const Header = () => (
   <H.Wrapper>
     <div className="container">
-      <div>logo</div>
-      <div>pesquisa</div>
+      <Link href="./">
+        <Image
+          src={Brand}
+          height={38}
+          width={177}
+          alt="Me Joga"
+          title="Me Joga"
+        />
+      </Link>
+      <form className="search">
+        <input type="text" placeholder="Pesquisar na loja" />
+        <button>
+          <img src="./images/search.svg" />
+        </button>
+      </form>
       <div>links úteis</div>
-      <div>interações</div>
+      <div className="actions">
+        <a>aa</a>
+        <a>aa</a>
+        <a>aa</a>
+      </div>
     </div>
   </H.Wrapper>
 )
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -2,10 +2,48 @@ import styled from 'styled-components'
 
 export const Wrapper = styled.header`
   width: 100%;
-  height: 3.125rem;
-  background-color: var(--primary);
+  height: 5rem;
+  background-color: var(--grey-400);
   .container {
+    display: grid;
+    align-items: center;
+    grid-template-columns: 1fr 23.688rem 17.375rem 1fr;
+    grid-column-gap: 1.25rem;
+    height: 100%;
     max-width: 74rem;
     margin: 0 auto;
+    .search {
+      position: relative;
+      width: 17.375rem;
+      height: 2.5rem;
+      input[type='text'] {
+        width: 100%;
+        height: 100%;
+        padding: 0 0 0 1.25rem;
+        color: var(--grey-200);
+        background-color: var(--grey-500);
+        border: none;
+        border-radius: 0.375rem;
+        font-weight: 600;
+        &:focus {
+          outline: none;
+        }
+      }
+      button {
+        position: absolute;
+        top: 50%;
+        right: 1.25rem;
+        height: 1.25rem;
+        width: 1.25rem;
+        margin-top: -0.625rem;
+        border: none;
+        background-color: transparent;
+      }
+    }
+    .actions {
+      display: flex;
+      justify-content: flex-end;
+      grid-column-gap: 0.625rem;
+    }
   }
 `
