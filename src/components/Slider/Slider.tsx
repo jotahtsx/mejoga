c50ff8ae04a5c67f44d3a8dc2456f88aea294bb9
jotahtsx <joaomanoelskai@gmail.com>
2023-01-31 20:51:29 -0300

    mejoga || Add - Styles Slider

--- src/components/Slider/Slider.tsx
+++ src/components/Slider/Slider.tsx
@@ -18,18 +18,57 @@ export const Slider = () => (
     className="mySwiper"
   >
     <SwiperSlide>
-      <S.Wrapper>
-        <div className="container">slider</div>
+      <S.Wrapper
+        style={{ backgroundImage: `url(./images/slider-bg-01.png)` }}
+        className="sliderItem"
+      >
+        <div className="container">
+          <div>
+            <span>Popular</span>
+            <h2>The last of US II</h2>
+            <p>
+              Vivencie a narrativa emocionante e os personagens
+              <br /> inesquecíveis em The Last of Us™, vencedor de mais
+              <br /> de 200 prêmios de Jogo do Ano.
+            </p>
+          </div>
+        </div>
       </S.Wrapper>
     </SwiperSlide>
     <SwiperSlide>
-      <S.Wrapper>
-        <div className="container">slider</div>
+      <S.Wrapper
+        style={{ backgroundImage: `url(./images/slider-bg-01.png)` }}
+        className="sliderItem"
+      >
+        <div className="container">
+          <div>
+            <span>Popular</span>
+            <h2>The last of US II</h2>
+            <p>
+              Vivencie a narrativa emocionante e os personagens
+              <br /> inesquecíveis em The Last of Us™, vencedor de mais
+              <br /> de 200 prêmios de Jogo do Ano.
+            </p>
+          </div>
+        </div>
       </S.Wrapper>
     </SwiperSlide>
     <SwiperSlide>
-      <S.Wrapper>
-        <div className="container">slider</div>
+      <S.Wrapper
+        style={{ backgroundImage: `url(./images/slider-bg-01.png)` }}
+        className="sliderItem"
+      >
+        <div className="container">
+          <div>
+            <span>Popular</span>
+            <h2>The last of US II</h2>
+            <p>
+              Vivencie a narrativa emocionante e os personagens
+              <br /> inesquecíveis em The Last of Us™, vencedor de mais
+              <br /> de 200 prêmios de Jogo do Ano.
+            </p>
+          </div>
+        </div>
       </S.Wrapper>
     </SwiperSlide>
   </Swiper>
--- src/components/Slider/styles.ts
+++ src/components/Slider/styles.ts
@@ -3,9 +3,37 @@ import styled from 'styled-components'
 export const Wrapper = styled.div`
   width: 100%;
   height: 37.5rem;
+  background-size: cover;
+  background-repeat: none;
   .container {
+    display: flex;
+    align-items: center;
     height: 100%;
     max-width: 74rem;
     margin: 0 auto;
+    span {
+      width: auto;
+      display: inline-block;
+      margin-bottom: 1.25rem;
+      padding: 7px 14px;
+      font-weight: 700;
+      color: var(--white);
+      background-color: var(--grey-200);
+      border-radius: 1.875rem;
+    }
+    h2 {
+      margin-bottom: 1.25rem;
+      font-size: 2.25rem;
+      font-weight: 900;
+      text-shadow: 1px 1px rgb(0 0 0 / 50%);
+      color: var(--white);
+    }
+    p {
+      font-size: 0.875rem;
+      font-weight: 500;
+      line-height: 1.5rem;
+      text-shadow: 1px 1px rgb(0 0 0 / 50%);
+      color: var(--white);
+    }
   }
 `
