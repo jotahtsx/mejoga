1f7f183f83644c68eb07e4249db7ef4b50ebc027
jotahdev <joaomanoelskai@gmail.com>
2025-02-02 21:14:10 -0300

    Roolback 3

--- .lintstagedrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  '*.{js,jsx,ts,tsx}': (filenames) => {
-    console.log('Arquivos formatados:', filenames)
-    return [
-      `npx prettier --write ${filenames.join(' ')}`,
-      `npx eslint --fix ${filenames.join(' ')}`,
-    ]
-  },
-}
