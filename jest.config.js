152e64375b71b5fac02dd45a4d0555bcd752f80a
jotahdev <joaomanoelskai@gmail.com>
2025-02-03 22:41:57 -0300

    Ajuste feito com lint-staged

--- jest.config.js
+++ jest.config.js
@@ -4,8 +4,8 @@ module.exports = {
   collectCoverage: true,
   collectCoverageFrom: ['src/**/*.ts(x)?'],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
-  modulePaths: ['<rootDir/src/>'],
+  modulePaths: ['<rootDir>/src/'],
   transform: {
-    '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }],
+    '^.+\\.(ts|tsx)$': 'ts-jest',
   },
 }
---
+++ src/Components/index.tsx
@@ -0,0 +1,7 @@
+const Main = () => (
+  <main>
+    <h1>MeJoga</h1>
+  </main>
+)
+
+export default Main
