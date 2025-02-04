8908ebfb78a9d9b8e5d1e95bc1af2463b6fad090
jotahdev <joaomanoelskai@gmail.com>
2025-02-03 22:59:48 -0300

    Ajuste feito com lint-staged

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,7 +19,7 @@ export default function Home() {
             </code>
             .
           </li>
-          <li>Save and see your changes instantly.</li>
+          <li>Olá</li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
