87bb7225947c9bef42ec072227c2e160dcc313b6
jotahdev <joaomanoelskai@gmail.com>
2025-02-04 21:40:14 -0300

    Criando arquivo de teste para o projeto

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,7 @@ module.exports = {
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
   testPathIgnorePatterns: ['/node_modules/', '/.next/'],
   collectCoverage: true,
-  collectCoverageFrom: ['src/**/*.ts(x)?'],
+  collectCoverageFrom: ['src/**/*.ts(x)?', '!src/app/**', '!src/lib/registry.tsx'],
   modulePaths: ['<rootDir>/src/'],
   transform: {
     '^.+\\.(ts|tsx|js|jsx)$': 'babel-jest', // Usar Babel para transformar arquivos
