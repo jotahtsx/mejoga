12e35890547af6344a0747ce57c219a09273b0da
jotahdev <joaomanoelskai@gmail.com>
2025-02-15 21:12:40 -0300

    Customizando as cores do strapi

--- mejoga-api/src/admin/app.tsx
+++ mejoga-api/src/admin/app.tsx
@@ -31,25 +31,26 @@ export default {
           primary700: "#77CDFF",  // Azul mais claro
       
           neutral0: "#0d102f",    // Fundo neutro escuro
-          neutral00: "#030415",   // Fundo ainda mais escuro
-          neutral100: "#131735",  // Cinza-azulado escuro para contrastes
+          neutral00: "#030415",
+          neutral100: "#131735",
       
-          danger100: "#0D92F4",   // Azul no alerta (diferente do padrão vermelho)
-          danger200: "#1B1D36",   // Cinza muito escuro
-          danger600: "#FFFFFF",   // Texto do alerta em branco
+          danger100: "#0D92F4",
+          danger200: "#1B1D36",
+          danger600: "#FFFFFF",
       
-          success100: "#1F9C47",  // Verde escuro para sucesso
-          success600: "#48E79E",  // Verde vibrante no hover
+          success100: "#1F9C47",
+          success600: "#48E79E",
       
-          warning100: "#C99100",  // Amarelo dourado para alertas
-          warning600: "#FFBD4A",  // Laranja suave para ênfase
+          warning100: "#C99100",
+          warning600: "#FFBD4A",
       
-          secondary100: "#0B3D91", // Azul mais escuro para detalhes
-          secondary600: "#8AB4F8", // Azul claro para realce
+          secondary100: "#0B3D91",
+          secondary600: "#8AB4F8",
         }
       }
     }
   },
+  tutorials: false,
   bootstrap() {
   },
 };
